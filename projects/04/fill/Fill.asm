2444c7bdbe695e35a9d806dd20bacc885c092863
yun_sunjin <yun_sunjin@cocone.co.jp>
2021-03-21 00:43:26 +0900

    a

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,65 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+    @KBD
+    D=M
+
+    // 0以上なら黒く塗りつぶす
+    @ON
+    D;JGT
+
+    // 0なら白
+    @OFF
+    0;JMP
+
+(ON)
+    // drow valueを追加
+    @R0
+    M=-1
+
+    // Draw
+    @DRAW
+    0;JMP
+
+(OFF)
+    @R0
+    M=0
+
+    @DRAW
+    0;JMP
+
+(DRAW)
+    // 実行する必要のあるblits数
+    @8191
+    D=A
+    @R1
+    M=D
+
+    (NEXT)
+        @R1
+        D=M
+        @pos
+        M=D
+        @SCREEN
+        D=A
+        @pos
+        M=M+D
+
+        @R0
+        D=M
+        @pos
+        A=M
+        M=D
+
+        @R1
+        D=M-1
+        M=D
+
+        @NEXT
+        D;JGE
+
+// 描画完了したらLoopに戻す
+@LOOP
+0;JMP
