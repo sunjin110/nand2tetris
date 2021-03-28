1609c3e6a7913eab6c45941cd62b430e606afeb7
yun_sunjin <sunjin110@gmail.com>
2021-03-28 22:50:32 +0900

    定義済みのあれを追加

--- assembler/pkg/symboltable/symboltable.go
+++ assembler/pkg/symboltable/symboltable.go
@@ -11,13 +11,43 @@ type SymbolTableMap map[string]int
 
 // GetSymbolTableMap アセンブラからsymbolTableを作成する
 // L命令のものはAdressは現在のread中の行数を読む
-// A命令の場合は、1024から開始する
+// 定義済みシンボルもここで追加しておく
 // ２つ目の引数には、symbolTableMapですでに割り当てられているアドレスかどうかを調べるためにつかう
 func GetSymbolTableMap(scanner *bufio.Scanner) (SymbolTableMap, map[int]bool) {
 
-	symbolTableMap := SymbolTableMap{}
-	var addressCnt int
+	// 定義済みシンボルを追加する
+	symbolTableMap := SymbolTableMap{
+		"SP":     0,
+		"LCL":    1,
+		"ARG":    2,
+		"THIS":   3,
+		"THAT":   4,
+		"R0":     0,
+		"R1":     1,
+		"R2":     2,
+		"R3":     3,
+		"R4":     4,
+		"R5":     5,
+		"R6":     6,
+		"R7":     7,
+		"R8":     8,
+		"R9":     9,
+		"R10":    10,
+		"R11":    11,
+		"R12":    12,
+		"R13":    13,
+		"R14":    14,
+		"R15":    15,
+		"SCREEN": 16384,
+		"KBD":    24576,
+	}
+
 	isUseAddressMap := map[int]bool{}
+	for _, address := range symbolTableMap {
+		isUseAddressMap[address] = true
+	}
+
+	var addressCnt int
 	for scanner.Scan() {
 
 		line := scanner.Text()
