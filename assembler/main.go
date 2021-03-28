a4f414a06fe6b296d03f1646dd138c0bab5f12da
yun_sunjin <sunjin110@gmail.com>
2021-03-28 15:40:32 +0900

    Assembler

---
+++ assembler/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+// .asmファイルを読み込んで、バイナリーコード(.hack)を作成する
+func main() {
+	log.Println("Assembler")
+
+	// 引数を取得する
+	flag.Parse()
+	args := flag.Args()
+	if len(args) == 0 {
+		fmt.Println("引数がありません")
+		os.Exit(1)
+	}
+
+	asmFileName := args[0]
+	fp, err := os.Open(asmFileName)
+	if err != nil {
+		fmt.Println("ファイルが見つかりません")
+		panic(err)
+	}
+	defer fp.Close()
+
+	scanner := bufio.NewScanner(fp)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+}
