9e1747b768a596cb7fa5370480e19d5fa6fa6698
yun_sunjin <sunjin110@gmail.com>
2021-04-29 22:20:44 +0900

    a

--- compiler/pkg/analyzer/analyzer.go
+++ compiler/pkg/analyzer/analyzer.go
@@ -4,12 +4,20 @@ import (
 	"compiler/pkg/common/chk"
 	"compiler/pkg/common/jsonutil"
 	"compiler/pkg/tokenizer"
+	"compiler/pkg/xmlwriter"
 	"fmt"
 )
 
 // Analyzer Xmlに変換する
 func Analyzer(outputFileName string, pathList []string) {
 	jsonutil.Print(pathList)
+
+	// xml writerを作成する
+	xmlWriter, err := xmlwriter.New(outputFileName)
+	chk.SE(err)
+	defer func() {
+		xmlWriter.Close()
+	}()
 
 	// コードをparseする
 	for _, path := range pathList {
@@ -24,6 +32,27 @@ func Analyzer(outputFileName string, pathList []string) {
 
 			token := t.Token
 			fmt.Println("token is ", token)
+
+			tokenType := tokenizer.GetTokenType(token)
+			switch tokenType {
+			case tokenizer.TokenTypeKeyWord:
+				// keyword := tokenizer.GetKeyWord(token)
+				xmlWriter.WriteToken(tokenType, token)
+			case tokenizer.TokenTypeSymbol:
+				symbol := t.GetSymbol()
+				xmlWriter.WriteToken(tokenType, string(symbol))
+			case tokenizer.TokenTypeIdentifier:
+				identifier := t.GetIdentifier()
+				xmlWriter.WriteToken(tokenType, identifier)
+			case tokenizer.TokenTypeIntConst:
+				intVal := t.GetIntVal()
+				xmlWriter.WriteToken(tokenType, fmt.Sprintf("%d", intVal))
+			case tokenizer.TokenTypeStringConst:
+				strVal := t.GetStringVal()
+				xmlWriter.WriteToken(tokenType, strVal)
+			default:
+				panic("定義されていないtokenType")
+			}
 
 		}
 
--- compiler/pkg/xmlwriter/xmlwriter.go
+++ compiler/pkg/xmlwriter/xmlwriter.go
@@ -3,6 +3,7 @@ package xmlwriter
 import (
 	"compiler/pkg/common/chk"
 	"compiler/pkg/tokenizer"
+	"fmt"
 	"os"
 )
 
@@ -15,20 +16,41 @@ type XmlWriter struct {
 func New(filePath string) (*XmlWriter, error) {
 	fp, err := os.Create(filePath)
 	chk.SE(err)
+
+	write(fp, "<tokens>\n")
 	return &XmlWriter{
 		file: fp,
 	}, nil
 }
 
 // WriteToken .
-func (x *XmlWriter) WriteToken(token string, tokenType tokenizer.TokenType, keyword tokenizer.KeyWord) {
+func (x *XmlWriter) WriteToken(tokenType tokenizer.TokenType, tokenVal string) {
+
+	value := tokenVal
+	switch value {
+	case "<":
+		value = "&lt;"
+	case ">":
+		value = "&gt;"
+	case "&":
+		value = "&amp;"
+	}
+
+	outLine := fmt.Sprintf("<%s>%s</%s>\n", tokenType, value, tokenType)
+	write(x.file, outLine)
 
 }
 
 // Close .
 func (x *XmlWriter) Close() {
 
-	// TODO write「</tokens>」
+	write(x.file, "</tokens>\n")
 
 	x.file.Close()
+}
+
+// 実際にfileに書き込む
+func write(file *os.File, outLine string) {
+	_, err := file.WriteString(outLine)
+	chk.SE(err)
 }
