3f2d5a8798c42e65419f5fe76d8c9051d2496557
yun_sunjin <yun_sunjin@cocone.co.jp>
2021-06-05 13:00:27 +0900

    本当はちょっと違うアプローチで実装するかも

--- compiler/pkg/symboltable/symboltable.go
+++ compiler/pkg/symboltable/symboltable.go
@@ -2,6 +2,28 @@ package symboltable
 
 // SymbolTable .
 type SymbolTable struct {
+	ClassName string
+	RowList   []*Row // シンボルテーブルのあれ
+	// TODO その中のscopeのものを設定できる必要がある
+
 }
 
-// Scope .
+// Row 一つのデータのまとまり
+type Row struct {
+	VarName   string // 変数名
+	Type      string // int | String | boolean | ClassName
+	Attribute string // static | field | argument | local . TODO 変数名変えてもいいかも
+	Num       int32  // 番号
+	// ???
+	SymbolTable *SymbolTable // これが
+}
+
+// // SymbolTable .
+// type SymbolTable struct {
+// 	VarName string // 変数名
+// 	Type    string // int | String | boolean | char
+
+// 	Num     int32  // 番号
+// }
+
+// // Scope .
