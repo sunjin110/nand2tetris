147c5f8785edbdc8f64aa549e9cd33988acac936
yun_sunjin <sunjin110@gmail.com>
2021-06-06 22:49:17 +0900

    Sevenはok

---
+++ compiler/pkg/writer/vmwriter/os.go
@@ -0,0 +1,18 @@
+package vmwriter
+
+// JackOSで標準で使用できるFunctionなどの定義
+
+// const (
+// 	// MathMultiply .
+// 	MathMultiply = "Math.multiply"
+// )
+
+// getMathMultiply 掛け算の関数
+func getMathMultiply() (string, int32) {
+	return "Math.multiply", 2
+}
+
+// getMathDivide 引き算の関数
+func getMathDivide() (string, int32) {
+	return "Math.divide", 2
+}
