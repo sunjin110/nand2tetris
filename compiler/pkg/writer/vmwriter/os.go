26a000635bc482ab1f542efbf5a9ddc26b217b5b
yun_sunjin <sunjin110@gmail.com>
2021-06-12 23:23:56 +0900

    fix: delete comment

--- compiler/pkg/writer/vmwriter/os.go
+++ compiler/pkg/writer/vmwriter/os.go
@@ -2,11 +2,6 @@ package vmwriter
 
 // JackOSで標準で使用できるFunctionなどの定義
 
-// const (
-// 	// MathMultiply .
-// 	MathMultiply = "Math.multiply"
-// )
-
 // getMathMultiply 掛け算の関数
 func getMathMultiply() (string, int32) {
 	return "Math.multiply", 2
