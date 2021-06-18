524357e02931aa45afa641c624c51198c6bd721f
yun_sunjin <yun_sunjin@cocone.co.jp>
2021-06-18 21:03:27 +0900

    a

--- jackos/pkg/output/output_test.go
+++ jackos/pkg/output/output_test.go
@@ -15,9 +15,15 @@ func Test(t *testing.T) {
 
 	g.Describe("output", func() {
 		g.It("test", func() {
-			fmt.Printf("%b\n", int16(255))
+			// fmt.Printf("%b\n", int16(255))
 			// fmt.Println(-1 & 255)
-			fmt.Printf("%b\n", int16(-1&255))
+			// fmt.Printf("%b\n")
+			fmt.Printf("%b\n", 0&1)
+			fmt.Printf("%b\n", 1&1)
+			fmt.Printf("%b\n", 2&1)
+			fmt.Printf("%b\n", 3&1)
+			fmt.Printf("%d", 102&1)
+
 		})
 	})
 
